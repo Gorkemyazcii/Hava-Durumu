0bcdf83fa26ecbe7ea84f67e1bd3f45ec3a9e2fd
Görkem <Gkrem33@gmail.com>
2023-09-17 10:28:17 +0300

    Enter çalıştır

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Gorkem Yazıcı");
+const url = 'https://api.openweathermap.org/data/2.5/';
+const key = 'fd9ba0d8f7ea885daf693586f3e84886';
+
+const setQuery = (e) => {
+    if (e.keyCode == '13')
+        getResult(searchBar.value)
+}
+const getResult = (cityName) => {
+    console.log(cityName);
+}
+
+const searchBar = document.getElementById('searchBar');
+searchBar.addEventListener('keypress', setQuery);
